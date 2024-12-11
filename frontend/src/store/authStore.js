f6474f65de8b7d5d95a92decf0638f2027737594
trquockiet24 <trquockiet24@gmail.com>
2024-12-11 14:55:44 +0700

    fix

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,10 @@ import { create } from "zustand";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const API_URL = "https://taskmanager-1l9s.onrender.com/api/auth";
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
