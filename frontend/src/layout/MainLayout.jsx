2ac225215899ba390be97783eecfe64c96eef852
trquockiet24 <trquockiet24@gmail.com>
2024-12-11 15:13:03 +0700

    fix

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-
-import SideBar from "../components/SideBar";
+import SideBar from "../components/Sidebar";
 
 const MainLayout = () => {
   return (
